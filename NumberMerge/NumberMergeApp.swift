f1ae100d096bdf88118f5daa74d68fdb0b864165
FanRende <zxcj04@gmail.com>
2022-04-24 07:07:17 +0800

    Initial Commit

---
+++ NumberMerge/NumberMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberMergeApp.swift
+//  NumberMerge
+//
+//  Created by FanRende on 2022/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NumberMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
